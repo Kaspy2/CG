a790f18bdae15e08a097a6b4aabf85fb8d671c3c
mrbartchris <bartolochristian13@gmail.com>
2018-03-14 19:29:02 +0100

    added hello world test

---
+++ Assignment/src/test/java/cps2002/assignment/cg/HelloWorldTests.java
@@ -0,0 +1,30 @@
+package cps2002.assignment.cg;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HelloWorldTests {
+
+    private HelloWorld helloWorld;
+
+    @Before
+    public void setup() {
+        helloWorld = new HelloWorld();
+    }
+
+    @After
+    public void teardown() {
+        helloWorld = null;
+    }
+
+    @Test
+    public void testSimpleGetMessage() {
+        //Exercise
+        String message = helloWorld.getMessage();
+
+        //Verify
+        assertEquals("Hello World", message);
+    }
+}
