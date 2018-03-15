5862a5692f9e71e29a989403a633bf2925d420fa
mrbartchris <bartolochristian13@gmail.com>
2018-03-15 14:08:06 +0100

    tests working

---
+++ src/main/java/game/HelloWorld.java
@@ -0,0 +1,7 @@
+package game;
+
+public class HelloWorld {
+  public String getMessage(){
+    return "Hello World";
+  }
+}
---
+++ src/test/java/game/HelloWorldTest.java
@@ -0,0 +1,30 @@
+package game;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+
+  private HelloWorld helloWorld;
+
+  @Before
+  public void setup() {
+    helloWorld = new HelloWorld();
+  }
+
+  @After
+  public void teardown() {
+    helloWorld = null;
+  }
+
+  @Test
+  public void testSimpleGetMessage() {
+    //Exercise
+    String message = helloWorld.getMessage();
+
+    //Verify
+    assertEquals("Hello World", message);
+  }
+}
