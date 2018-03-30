338b5d69aa43eff885a1edec6a7f00de2374b1e9
mrbartchris <bartolochristian13@gmail.com>
2018-03-30 17:40:42 +0200

    add skeleton classes

---
+++ src/main/java/game/Game.java
@@ -0,0 +1,40 @@
+package game;
+
+import java.util.*;
+
+public class Game{
+    private int turns;
+    private ArrayList<Player> players;
+    private Map map;
+
+    public Game(){
+        turns = 0;
+        players = new ArrayList<Player>();
+        map = new Map(0);
+    }
+
+    public void setTurns(int turns){
+        this.turns = turns;
+    }
+
+    public void setMap(Map map){
+        this.map = map;
+    }
+
+    public int getTurns(){
+        return this.turns;
+    }
+
+    public ArrayList<Player> getPlayers(){
+        return this.players;
+    }
+
+    public Map getMap(){
+        return this.map;
+    }
+
+    public static void main (String[] args){}
+    public void startGame(){}
+    public boolean setNumPlayers(int n){ return false; }
+    public void generateHTMLFiles(){}
+}
---
+++ src/main/java/game/Player.java
@@ -0,0 +1,12 @@
+package game;
+
+class Player{
+    Coordinate coordinate;
+
+    public Player(){
+        coordinate = new Coordinate();
+    }
+
+    public void move(char direction){}
+    public boolean setCoordinate(Coordinate coordinate){ return false; }
+}
