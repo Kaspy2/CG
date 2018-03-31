32841b3a8853ef44a92fd84aa2f4a1c395f65aa4
mrbartchris <bartolochristian13@gmail.com>
2018-03-31 17:34:44 +0200

    start map

--- src/main/java/game/Map.java
+++ src/main/java/game/Map.java
@@ -1,16 +1,47 @@
 package game;
 
-public class Map{
-    private int size;
+import java.util.*;
 
-    public Map(int size){
-        this.size = size;
+public class Map {
+    private int size;
+    private char[][] map;
+    private static double water_rate;
+
+    public Map(){
+        this.size = 0;
+        this.map = new char[this.size][this.size];
+        water_rate = 40;
+    }
+
+    public boolean setMapSize(int size){
+        if(size >= 5 && size <= 50){
+            this.size = size;
+            return true;
+        }
+        return false;
     }
 
     public int getSize(){
         return this.size;
     }
 
-    public void generateMap(){}
-    public char getTileType(int x, int y){ return ' '; }
+    public void generateMap(){
+        Random rand = new Random();
+
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                if((rand.nextInt(100) + 1) < water_rate){
+                    map[i][j] = 'w';
+                } else {
+                    map[i][j] = 'g';
+                }
+            }
+        }
+
+        map[rand.nextInt(size)][rand.nextInt(size)] = 't';
+    }
+
+    public char getTileType(int x, int y){
+        return map[x][y];
+    }
 }
