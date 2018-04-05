af97aca324c25773e4bf544a39577dcaae933b71
Gabriel Farrugia <gfarrug@gmail.com>
2018-04-05 19:09:32 +0200

    Update PlayerTest and Player

--- src/main/java/game/Player.java
+++ src/main/java/game/Player.java
@@ -1,12 +1,47 @@
 package game;
 
 class Player{
-    Coordinate coordinate;
+    private Coordinate coordinate;
 
-    public Player(){
-        coordinate = new Coordinate(0, 0);
+    public void move(char direction){
+    	int x = this.coordinate.getX();
+    	int y = this.coordinate.getY();
+
+    	if(direction == 'u') {
+    		this.setCoordinate(new Coordinate(x, y+1));
+    	}
+    	else if (direction == 'd') {
+    		this.setCoordinate(new Coordinate(x, y-1));
+    	}
+    	else if (direction == 'l') {
+    		this.setCoordinate(new Coordinate(x-1, y));
+    	}
+    	else if (direction == 'r') {
+    		this.setCoordinate(new Coordinate(x+1, y));
+    	}
+    	// else should be programmatically avoided
+    	// else does not change coordinates since invalid move direction
+    	else {
+    		System.out.println("Error: Wrong direction type!");
+    		// throw MoveException();
+    	}
     }
 
-    public void move(char direction){}
-    public boolean setCoordinate(Coordinate coordinate){ return true; }
+    public boolean setCoordinate(Coordinate coordinate){ 
+    	// must always set to a different coordinate
+    	if (this.coordinate == coordinate) {
+    		return false;
+    	}
+    	// coordinates must always be in positive sides of both dimensions
+    	if (coordinate.getX() < 0 || coordinate.getY() < 0) {
+    		return false;
+    	}
+
+		this.coordinate = coordinate;
+		return true;
+    }
+
+    public Coordinate getCoordinate() {
+    	return this.coordinate;
+    }
 }
--- src/test/java/game/PlayerTest.java
+++ src/test/java/game/PlayerTest.java
@@ -21,10 +21,74 @@ public class PlayerTest{
 	@Test
 	public void testPlayerSetCoordinates(){
 		// Exercise
-		Coordinate c = new Coordinate(0,0);
+		Coordinate c = new Coordinate(4,4);
 
 		// Verify
 		assertEquals(true,player.setCoordinate(c));
+	}
+
+	@Test
+	public void testPlayerSetNegativeCoordinates(){
+		// Exercise
+		Coordinate c = new Coordinate(-1,-1);
+
+		// Verify
+		assertEquals(false,player.setCoordinate(c));
+	}
+
+	@Test
+	public void testPlayerSetSameCoordinates(){
+		// Exercise
+		Coordinate c = new Coordinate(10,20);
+		player.setCoordinate(c);
+
+		// Verify
+		assertEquals(false,player.setCoordinate(c));
+	}
+
+	@Test
+	public void testPlayerMoveUp(){
+		player.setCoordinate(new Coordinate(1,1));
+		Coordinate testCoord = new Coordinate(1,2);
+		player.move('u');
+
+		assertEquals(testCoord, player.getCoordinate());
+	}
+
+	@Test
+	public void testPlayerMoveDown(){
+		player.setCoordinate(new Coordinate(1,1));
+		Coordinate testCoord = new Coordinate(1,0);
+		player.move('d');
+
+		assertEquals(testCoord, player.getCoordinate());
+	}
+
+	@Test
+	public void testPlayerMoveLeft(){
+		player.setCoordinate(new Coordinate(1,1));
+		Coordinate testCoord = new Coordinate(0,1);
+		player.move('l');
+
+		assertEquals(testCoord, player.getCoordinate());
+	}
+
+	@Test
+	public void testPlayerMoveRight(){
+		player.setCoordinate(new Coordinate(1,1));
+		Coordinate testCoord = new Coordinate(2,1);
+		player.move('r');
+
+		assertEquals(testCoord, player.getCoordinate());
+	}
+
+	@Test
+	public void testPlayerMoveInvalidDirection(){
+		Coordinate testCoord = new Coordinate(1,2);
+		player.setCoordinate(testCoord);
+		player.move('a');
+
+		assertEquals(testCoord, player.getCoordinate());
 	}
 
 }
